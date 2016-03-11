58894c29aa2e70a675decb1830f56cb2785d53b7
hzxiejin <hzxiejin@corp.netease.com>
2016-03-11 21:02:15 +0800

    add nei proj

---
+++ main/nei.11356/route.js
@@ -0,0 +1,16 @@
+var u = require('./util.js');
+
+module.exports = {
+    "GET /api/account/authority/get":u.p(15278,"get/api/account/authority/get"),
+    "POST /api/account/authority/get":u.p(15281,"post/api/account/authority/get"),
+    "GET /api/account/info/get":u.p(15277,"get/api/account/info/get"),
+    "POST /api/account/info/get":u.p(15280,"post/api/account/info/get"),
+    "POST /api/blog/delete":u.p(15274,"post/api/blog/delete"),
+    "GET /api/blog/list":u.p(15273,"get/api/blog/list"),
+    "GET /api/tag/list":u.p(15276,"get/api/tag/list"),
+    "GET /blog/list/":u.r(0,[{"i":10947,"p":"index"}]),
+    "GET /blog/tag/":u.r(0,[{"i":10947,"p":"index"}]),
+    "GET /setting/account/":u.r(0,[{"i":10947,"p":"index"}]),
+    "GET /setting/account/edu/":u.r(0,[{"i":10947,"p":"index"}]),
+    "GET /setting/permission/":u.r(0,[{"i":10947,"p":"index"}])
+};
